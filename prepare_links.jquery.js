773e51d8bc69167ab3bc0d987ad10651cdd7f7a2
Lee Jones <lee.jones@digitalscientists.com>
2009-08-17 17:15:36 -0400

    Initial commit

---
+++ prepare_links.jquery.js
@@ -0,0 +1,17 @@
+jQuery.fn.prepare_links = function() {
+
+	var current_host = new RegExp(window.location.host);
+
+	return this.each(function(){
+		a = $(this);
+
+		if (a.attr('href') != undefined && a[0].host != undefined) {
+			host = a[0].host;
+
+			// external links should open in new window
+			if(!current_host.test(host)) {
+				a.attr({ target : "_blank" });
+			}
+		}
+	});
+};
