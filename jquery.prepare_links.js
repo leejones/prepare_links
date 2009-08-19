21630259306360648f207c739e8f2d1c544df46a
Lee Jones <lee.jones@digitalscientists.com>
2009-08-19 17:53:50 -0400

    Namespaced plugin to be compatible with jquery no conflict

--- jquery.prepare_links.js
+++ jquery.prepare_links.js
@@ -1,17 +1,19 @@
-jQuery.fn.prepare_links = function() {
+jQuery(function($) {
+	jQuery.fn.prepare_links = function() {
 
-	var current_host = new RegExp(window.location.host);
+		var current_host = new RegExp(window.location.host);
 
-	return this.each(function(){
-		a = $(this);
+		return this.each(function(){
+			a = $(this);
 
-		if (a.attr('href') != undefined && a[0].host != undefined) {
-			host = a[0].host;
+			if (a.attr('href') != undefined && a[0].host != undefined) {
+				host = a[0].host;
 
-			// external links should open in new window
-			if(!current_host.test(host)) {
-				a.attr({ target : "_blank" });
+				// external links should open in new window
+				if(!current_host.test(host)) {
+					a.attr({ target : "_blank" });
+				}
 			}
-		}
-	});
-};
+		});
+	};
+});
