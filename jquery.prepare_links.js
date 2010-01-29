4e98e52c2454802ddf5fd1e21aadc5e88b4379bc
Lee Jones <scribblethink@gmail.com>
2010-01-29 00:06:38 -0500

    Added options for opening subdomains in a new window and adding sites that should not open in a new window.
    Added test for setting an internal site
    
    Subdomains can now be set to internal or external
    
    Removed unused variable
    
    Changed localhost method to is_localhost
    
    Change  variable to link
    
    Changed other_internals variable to other_internal_sites
    
    Moved internal links regex builder outside the element loop
    
    Removed subdomains are external function and changed api specifying subdomains_are_external
    
    Refactor initial setting of vars
    
    Renamed the other_internal_sites_result var to is_internal_site

--- jquery.prepare_links.js
+++ jquery.prepare_links.js
@@ -1,18 +1,38 @@
 jQuery(function($) {
-	jQuery.fn.prepare_links = function() {
-
-		var current_host = new RegExp(window.location.host);
-		var localhost = RegExp('localhost');
+	jQuery.fn.prepare_links = function(user_options) {
+		var options = {
+			subdomains_are_external : false,
+			additional_internal_sites : []
+		};
+		$.extend(options, user_options);
+		var current_host = new RegExp(window.location.host),
+				is_localhost = new RegExp('localhost'),
+				has_subdomain = new RegExp('(?:https?:\/\/)?.*\.' + location.host),
+				other_internal_sites = [];
+		
+		$.each(options.additional_internal_sites, function(index, element) {
+			var domain = element.replace(/(https?):\/\//, '' );
+			other_internal_sites.push(new RegExp(domain));
+		});
 		
 		return this.each(function(){
-			a = $(this);
+			
+			var link = $(this),
+					host = link[0].host,
+					is_internal_site = false;
+			
+			$.each(other_internal_sites, function(index, element) {
+				if (element.test(host)) {
+					is_internal_site = true;
+				}
+			});
 
-			if (a.attr('href') != undefined && a[0].host != undefined) {
-				var host = a[0].host;
-
-				// external links should open in new window
-				if(!current_host.test(host) && !localhost.test(host)) {
-					a.attr({ target : "_blank" });
+			if (link.attr('href') != undefined && link[0].host != undefined) {
+				if(!current_host.test(host) && !is_localhost.test(host) && !is_internal_site) {
+					link.attr({ target : "_blank" });
+				}
+				else if (has_subdomain(host) && options.subdomains_are_external) {
+					link.attr({ target : "_blank" });
 				}
 			}
 		});
--- tests/unit-tests.js
+++ tests/unit-tests.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	test("Basic plugin tests", function() {
-		ok($("a").prepare_links(), "Should respond as jQuery plugin.");
+		ok($("a").prepare_links(), "Should immediately respond as a jQuery plugin");
 	});
 	
 	test("Should open external links in new window.", function() {
@@ -8,6 +8,7 @@ $(document).ready(function() {
 		UnitTest.setup.links([
 			{id : 'google', url : 'http://google.com'},
 			{id : 'current_domain', url : window.location.host},
+			{id : 'current_domain', url : 'www' + window.location.host},
 			{id : 'relative_path', url : 'relative/path/'},
 			{id : 'absolute_path', url : '/absolute/path/'},
 			{id : 'subdomain', url : 'http://subdomain' + window.location.host},
@@ -20,6 +21,7 @@ $(document).ready(function() {
 		equal($("#google").attr('target'), "_blank", "Target should be _blank");
 
 		equal($("#current_domain").attr('target'), "", "Target should be empty for current domain");
+		equal($("#subdomain").attr('target'), "", "Target should be empty for current subdomain");
 		equal($("#relative_path").attr('target'), "", "Target should be empty for relative path");
 		equal($("#absolute_path").attr('target'), "", "Target should be empty for absolute path");
 		equal($("#localhost3000").attr('target'), "", "Target should be empty for local host with subdomain");
@@ -28,4 +30,33 @@ $(document).ready(function() {
 		
 		UnitTest.teardown.sandbox();
 	});
+
+	test("Should provide options for additional \"internal\" links.", function() {
+		UnitTest.setup.sandbox();
+		UnitTest.setup.links([
+			{id : 'affiliate_site', url : 'http://affiliatesite.com'}
+		]);
+		
+		$("#sandbox a").prepare_links({additional_internal_sites : ['http://affiliatesite.com']});
+		
+		equal($("#affiliate_site").attr('target'), "", "Target should be empty");
+		
+		UnitTest.teardown.sandbox();
+	});
+
+	test("Should allow subdomains to be considered external links.", function() {
+			UnitTest.setup.sandbox();
+			UnitTest.setup.links([
+				{id : 'affiliate_site', url : 'http://blog.' + location.host}
+			]);
+			
+			$("#sandbox a").prepare_links({subdomains_are_external : false});
+			equal($("#affiliate_site").attr('target'), "", "Target should be not be _blank");
+
+			$("#sandbox a").prepare_links({subdomains_are_external : true});
+			equal($("#affiliate_site").attr('target'), "_blank", "Target should be _blank");
+			
+			UnitTest.teardown.sandbox();
+	});
+
 });
